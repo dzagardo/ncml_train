3beee1ac25b56d86a6bd99539654f9b6117e1902
David Zagardo <dzagardo@alumni.cmu.edu>
2024-02-06 13:16:25 -0600

    reducing batch size again

--- train.py
+++ train.py
@@ -357,16 +357,16 @@ fp16 = False
 bf16 = False
 
 # Batch size per GPU for training
-per_device_train_batch_size = 2
+per_device_train_batch_size = 1
 
 # Batch size per GPU for evaluation
-per_device_eval_batch_size = 2
+per_device_eval_batch_size = 1
 
 # Micro batch size
 micro_batch_size = 1
 
 # Number of update steps to accumulate the gradients for
-gradient_accumulation_steps = 2
+gradient_accumulation_steps = 1
 
 # Enable gradient checkpointing
 gradient_checkpointing = True
